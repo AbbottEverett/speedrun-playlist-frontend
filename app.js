06193d86a608c42f889f4bcfdbecfcc82d713980
S. Everett Abbott <severettabbott@gmail.com>
2018-02-14 13:47:35 -0800

    Changed app.js to default view

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 (function() {
-  let currentView = window.views[1];
-  let selectedUser = 'Admin';
+  let currentView = window.views[0];
+  let selectedUser;
   window.changeView(currentView, selectedUser);
 
   window.currentView = currentView;
