7d79d62c9c1fcf943e3d53d828d4cf82d3c53b07
S. Everett Abbott <severettabbott@gmail.com>
2018-02-14 09:42:23 -0800

    Added basic view change system

--- app.js
+++ app.js
@@ -1,31 +1,26 @@
 (function() {
-  const root = document.getElementById('root');
-  window.getAllRuns()
-    .then((res) => {
-      console.log(res.data.data);
-    })
-    .catch((err) => {
-      console.log(err)
-    });
-  window.getRunById(1)
-    .then((res) => {
-      console.log(res.data.data);
-    })
-    .catch((err) => {
-      console.log(err)
-    });
-  window.getAllRunsFromAPlaylist(1)
-    .then((res) => {
-      console.log(res.data.data);
-    })
-    .catch((err) => {
-      console.log(err)
-    });
-  window.getARunByIdFromAPlaylist(1,1)
-    .then((res) => {
-      console.log(res.data.data);
-    })
-    .catch((err) => {
-      console.log(err)
-    });
+  // const root = document.getElementById('root');
+  const views = ['HOME', 'USER', 'PLAYLIST', 'RUN'];
+  let selectedUser;
+  let currentView = views[0];
+  const subtitle = document.getElementById('subtitle');
+
+  function changeView(view) {
+    switch(view) {
+      case 'HOME':
+        subtitle.textContent = 'Select A User';
+        break;
+      case 'USER':
+        subtitle.textContent = 'Choose A Playlist';
+        break;
+      case 'PLAYLIST':
+        subtitle.textContent = 'Pick A Run';
+        break;
+      case 'RUN':
+        break;
+      default:
+        console.log('Something is wrong!');
+    }
+  }
+  changeView(currentView);
 })();
